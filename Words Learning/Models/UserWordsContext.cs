c284c3570ed1ae7f8cca8a334dcb59418bfde66e
ipenk <ipenk@DESKTOP-NFO8I4L>
2021-10-28 21:13:20 +0300

    Add project files.

---
+++ Words Learning/Models/UserWordsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Words_Learning.Models
+{
+    public class UserWordsContext : DbContext
+    {
+        public DbSet<Words> Words { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public UserWordsContext(DbContextOptions<UserWordsContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Words Learning/Models/Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Words_Learning.Models
+{
+    public class Words
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Transcriptions { get; set; }
+        public string Translation { get; set; }
+        public string Image { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
